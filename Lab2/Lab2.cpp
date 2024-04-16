ffd28e3faf77a44261607bff316e11ac166d0371
igniteignis <ignat.bastrikov@mail.ru>
2024-04-16 16:46:26 +0700

    Lb2 complete...but

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -4,9 +4,24 @@
 
 int main()
 {
-    Mystring string("main.txt");//нужно переделать конструктор через get и addstring
-    string.CountUniqueWords();
-    //std::cout << string.FindSubstring("q") << std::endl;
+    setlocale(LC_ALL, "Russian");
+    Mystring string("add_to_file.txt");
+    WordCount* results;
+    int size = string.CountUniqueWords(&results);
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << (*results)[i].word << ": " << (*results)[i].count << "\n";
+    }
+    string.SortByFrequency(&results, size, true);
+    string.OutputDictionary_to_txt("sorted_dictionary.txt", &results, size);
+
+    //string.SortByFrequency(&results, size, false);
+
+    //int uniqueWordCount = string.CountUniqueWords(&results); // Вызываем метод CountUniqueWords
+    //string.ShowString();
+
+
+    //std::cout << string.FindSubstring("пошли?") << std::endl;
     //std::cout << string.CountWords();
     //Mystring string1("add_to_file.txt");
     //string1.ShowString();
